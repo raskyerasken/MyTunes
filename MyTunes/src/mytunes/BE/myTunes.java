42055ef11dc63a29d8850c0c15874cd714c0fb07
liverpoljer <liverpoljer@live.dk>
2017-11-30 12:59:41 +0100

    connect to database

---
+++ MyTunes/src/mytunes/BE/myTunes.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mytunes.BE;
+
+import java.util.ArrayList;
+import java.util.List;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.geometry.Side;
+import mytunes.GUI.AddSongController;
+
+/**
+ *
+ * @author mr.Andersen
+ */
+public class myTunes {
+    List<AddSongController> allSongs = new ArrayList();
+
+    private final StringProperty  songName =  new SimpleStringProperty();
+    private final StringProperty artist= new SimpleStringProperty();
+    private final StringProperty album =  new SimpleStringProperty();
+    private final IntegerProperty year= new SimpleIntegerProperty();
+    private final StringProperty path =  new SimpleStringProperty();
+    
+
+   /* public myTunes(String songName, String artist, String album, int year, String path) {
+       this.songName= new SimpleStringProperty();
+       this.artist = new SimpleStringProperty(); 
+       this.album = new SimpleStringProperty();
+       this.year = new SimpleIntegerProperty();
+      this.path = new SimpleStringProperty();
+        
+        this.songName.set(songName);
+        this.artist.set(artist);
+        this.album.set(album);
+        this.year.set(year);
+        this.path.set(path);
+    }*/
+
+    
+
+    public String getSongName()
+    {
+        return songName.get();
+    }
+    
+    public void setSongName(String songName)
+    {
+        
+        this.songName.set(songName);
+    }
+    public StringProperty songNameProperty(){
+    return songName;
+    }
+    
+    public String getArtist()
+    {
+        return artist.get();
+    }
+    public void setArtist(String artist)
+    {
+        this.artist.set(artist);
+    }   
+     public StringProperty ArtistProperty(){
+    return artist;
+    }
+    
+    public String getAlbum()
+    {
+        return album.get();
+    }
+    
+    public void setAlbum(String album)
+    {
+        this.album.set(album);
+    }
+    public StringProperty albumProperty(){
+    return album;
+    }
+    
+    public int getYear()
+    {
+        return year.get();
+    }
+    
+    public void setYear(int Year){
+        this.year.set(Year);
+           
+    }
+    public IntegerProperty yearProperty(){
+    return year;
+    }
+    
+    public String getPath() {
+        return path.get();
+    }
+    
+    public void setPath(String path) {
+        this.path.set(path);
+    }
+    public StringProperty pathProperty(){
+    return path;
+    }
+        
+
+
+ 
+    
+   
+    
+    @Override
+    public String toString() {
+        return "myTunes{" + "songName=" + songName + ", artist=" + artist + ", album=" + album + ", year=" + year + ",path="+path+'}';
+    }
+
+    
+    
+}
---
+++ MyTunes/src/mytunes/DAL/myTunesDAL.java
@@ -0,0 +1,136 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mytunes.DAL;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Platform;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.media.MediaPlayer;
+import mytunes.BE.myTunes;
+import mytunes.BLL.BLLManager;
+import mytunes.GUI.AddSongController;
+
+/**
+ *
+ * @author mr.Andersen
+ */
+
+            
+public class myTunesDAL {
+    private ConnectionManager cm = new ConnectionManager();
+
+    public List<AddSongController> getAllSongsByPlaylist(String songName, String Artist, String Album, int Year)
+    {
+        List<AddSongController> allSongs = new ArrayList();
+        
+    
+        {
+            AddSongController s = new AddSongController();
+            allSongs.add(s);
+        }
+        
+        return allSongs;
+    }
+    
+    public List<AddSongController> getAllSongs(String songName, String Artist, String Album, int Year)
+    {
+        List<AddSongController> allSongs = new ArrayList();
+        {
+            AddSongController s = new AddSongController();
+            allSongs.add(s);
+            
+        }
+        return allSongs;
+    }
+    
+    public void remove(myTunes selectedSong)
+    {
+        try (Connection con = cm.getConnection()) {
+        String sql = "DELETE FROM Songs WHERE id=?";
+        
+        PreparedStatement pstmt = con.prepareStatement(sql);
+        pstmt.setString(1, selectedSong.getSongName());
+        pstmt.execute();
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(myTunesDAL.class.getName()).log(Level.SEVERE, null, ex);
+            
+        }
+    }
+    
+    public void add (myTunes allSongs) throws SQLException
+    {
+                 try (Connection con = cm.getConnection())  {
+
+        String sql 
+                = "INSERT INTO Songs"
+                + "(songName, artist, album, year)"
+                + "VALUES(?, ?, ?, ?)";
+           PreparedStatement pstmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+           
+            pstmt.setString(1, allSongs.getSongName());
+            pstmt.setString(2, allSongs.getArtist());
+            pstmt.setString(3, allSongs.getAlbum());
+            pstmt.setInt(4, allSongs.getYear());
+            
+            int affected = pstmt.executeUpdate();
+            if (affected<1)
+                    throw new SQLException("Song could not be added");
+           
+                 }
+    catch (SQLException ex) {
+        Logger.getLogger(myTunesDAL.class.getName()).log(Level.SEVERE, null, ex);
+    }     
+  }
+
+    public List<myTunes> getAllSong() {
+      
+        List<myTunes> allSong
+                = new ArrayList();
+
+        try (Connection con = cm.getConnection()) {
+            PreparedStatement stmt
+                    = con.prepareStatement("SELECT * FROM bestTunesTable");
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                myTunes s = new myTunes();
+                s.setSongName(rs.getString("Name"));
+                s.setAlbum(rs.getString("Album"));
+                s.setArtist(rs.getString("Artist"));
+                s.setYear(rs.getInt("Year"));
+                                
+
+                allSong.add(s);
+            }
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(myTunesDAL.class.getName()).log(
+                    Level.SEVERE, null, ex);
+        }
+        return allSong;
+    
+    }
+}
+    
+  
+    
+    
+ 
+
