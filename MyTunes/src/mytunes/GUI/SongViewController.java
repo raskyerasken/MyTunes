de93bf7f34188a60bc6a7bfe1dabad2b7be1593f
liverpoljer <liverpoljer@live.dk>
2017-12-01 12:11:28 +0100

    change new song

--- MyTunes/src/mytunes/GUI/SongViewController.java
+++ MyTunes/src/mytunes/GUI/SongViewController.java
@@ -8,21 +8,15 @@ package mytunes.GUI;
 import com.mpatric.mp3agic.InvalidDataException;
 import com.mpatric.mp3agic.NotSupportedException;
 import com.mpatric.mp3agic.UnsupportedTagException;
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
 import mytunes.BE.TrackUtility;
 import mytunes.BE.MyTunes;
@@ -34,26 +28,16 @@ import mytunes.BE.MyTunes;
  */
 public class SongViewController implements Initializable {
 
-    @FXML
-    private TextField textTitle;
-    @FXML
-    private TextField textArtist;
-    @FXML
-    private TextField textFile;
-    @FXML
-    private TextField textAlbum;
-    @FXML
-    private TextField textYear;
+    
     @FXML
     private Button chooseDirectory;
 
-    private Stage dialogStage;
-    private MyTunes trackList;
-    private boolean okClicked = false;
-     MyTunes songData ;
+    MyTunes songData ;
     MyTunesModel model = new MyTunesModel();
     @FXML
     private Button closeButton;
+    @FXML
+    private TextField txtCategory;
     
     /**
      * Initializes the controller class.
@@ -62,44 +46,31 @@ public class SongViewController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    @FXML
-    private void save(ActionEvent event) throws SQLException {
-      model.add(songData);
-      MyTunesController MTModel = new MyTunesController();
-      //MTModel.playlistUpdate();
-      
-    }
-    
     
 
     @FXML
-    private void chooseDirectory(ActionEvent event) throws UnsupportedTagException, InvalidDataException, IOException, NotSupportedException 
+    private void chooseDirectory(ActionEvent event) throws UnsupportedTagException, InvalidDataException, IOException, NotSupportedException, SQLException 
     {
          TrackUtility  choser= new TrackUtility();
         songData = choser.getdata();
-         
         System.out.println(songData.toString());
-        textTitle.setText(songData.getSongName().toString());
-       textArtist.setText(songData.getArtist().toString());
-        textFile.setText(songData.getPath().toString());
-        textYear.setText(""+songData.getYear());
-        textAlbum.setText(songData.getAlbum().toString());
+      model.add(songData);
+      MyTunesController MTModel = new MyTunesController();
+        Stage stage = (Stage) chooseDirectory.getScene().getWindow();
+    // do what you have to do
+    stage.close();
     }
 
     @FXML
-<<<<<<< HEAD
-    private void closeSongview(ActionEvent event) {
+private void closeSongview(ActionEvent event) {
          Stage stage = (Stage) closeButton.getScene().getWindow();
     // do what you have to do
     stage.close();
     }
 
    
-=======
-    private void cancelButton(ActionEvent event) {
->>>>>>> bd5879170e7fefbbfa8ee2db3ec784a3f98183ea
-    }
 
+    
    
 }
 
