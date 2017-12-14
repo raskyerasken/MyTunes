6968d0267230b1c441aa99121e2f737c17c25b76
raskyerasken <32293919+raskyerasken@users.noreply.github.com>
2017-12-14 16:02:52 +0100

    hey

--- MyTunes/src/mytunes/GUI/PlayListController.java
+++ MyTunes/src/mytunes/GUI/PlayListController.java
@@ -14,7 +14,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import mytunes.BE.MyTunes;
 import mytunes.BE.Playlist;
 
 /**
